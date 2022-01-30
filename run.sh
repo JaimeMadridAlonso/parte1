3d9498cc120002b79bd64b241241b71c4ca91234
JaimeMadridAlonso <36418914+JaimeMadridAlonso@users.noreply.github.com>
2022-01-30 19:01:52 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get -y install python3-pip
+#sudo apt-get update
+#sudo apt-get -y install python3-pip
 
 export GROUP_NUMBER=48
 
-sudo rm -r practica_creativa2
+#sudo rm -r practica_creativa2
+rm -r practica_creativa2
 
 python3 parte1.py
